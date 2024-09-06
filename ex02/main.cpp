cff7e2397c136f729c1bed84df3a2972f0599055
jcummins <jcummins42prague@gmail.com>
2024-09-06 14:42:42 +0200

    Fixed demonstrations in ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 17:43:42 by jcummins          #+#    #+#             */
-/*   Updated: 2024/09/06 14:14:48 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/09/06 14:42:19 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,73 @@
 
 void	test_comparison_operators(Fixed &f1, Fixed &f2)
 {
+	std::cout	<< "Demonstrating comparison operators:" << std::endl;
 	test_greater(f1, f2);
 	test_greater_eq(f1, f2);
 	test_lesser(f1, f2);
 	test_lesser_eq(f1, f2);
 	test_equal(f1, f2);
 	test_unequal(f1, f2);
+	std::cout	<< std::endl;
 }
 
 void	test_arithmetic_operators(const Fixed &f1, const Fixed &f2)
 {
-	std::cout << f1 << " + " << f2 << " = " << f1 + f2 << std::endl;
-	std::cout << f1 << " - " << f2 << " = " << f1 - f2 << std::endl;
-	std::cout << f1 << " * " << f2 << " = " << f1 * f2 << std::endl;
-	std::cout << f1 << " / " << f2 << " = " << f1 / f2 << std::endl;
+	std::cout	<< "Demonstrating arithmetic operators:" << std::endl
+				<< "\t" << f1 << " + " << f2 << " = " << f1 + f2 << std::endl
+				<< "\t" << f1 << " - " << f2 << " = " << f1 - f2 << std::endl
+				<< "\t" << f1 << " * " << f2 << " = " << f1 * f2 << std::endl
+				<< "\t" << f1 << " / " << f2 << " = " << f1 / f2 << std::endl
+				<< std::endl;
 }
 
 void	test_minmax(const Fixed &f1, const Fixed &f2)
 {
-	std::cout << "max is" << Fixed::max(f1, f2) << std::endl;
-	std::cout << "min is" << Fixed::min(f1, f2) << std::endl;
+	std::cout	<< "Demonstrating toInt:" << std::endl
+				<< "\tmax is" << Fixed::max(f1, f2) << std::endl
+				<< "\tmin is" << Fixed::min(f1, f2) << std::endl
+				<< std::endl;
+}
+
+void	test_show_int(const Fixed &a, const Fixed &b, const Fixed &c, const Fixed &d)
+{
+	std::cout	<< "Demonstrating toInt:" << std::endl;
+	std::cout 	<< "\ta is " << a.toInt() << " as integer" << std::endl
+				<< "\tb is " << b.toInt() << " as integer" << std::endl
+				<< "\tc is " << c.toInt() << " as integer" << std::endl
+				<< "\td is " << d.toInt() << " as integer" << std::endl
+				<< std::endl;
+}
+
+void	test_show_fl(const Fixed &a, const Fixed &b, const Fixed &c, const Fixed &d)
+{
+	std::cout	<< "Demonstrating << overload and toFloat:" << std::endl
+				<< "\ta is " << a
+				//<< " with raw bits " << a.getRawBits()
+				<< std::endl
+				<< "\tb is " << b
+				//<< " with raw bits " << b.getRawBits()
+				<< std::endl
+				<< "\tc is " << c
+				//<< " with raw bits " << c.getRawBits()
+				<< std::endl
+				<< "\td is " << d
+				//<< " with raw bits " << d.getRawBits()
+				<< std::endl
+				<< std::endl;
+}
+
+void	test_incr_decr(Fixed &f1)
+{
+	std::cout	<< "Demonstrating increment / decrement:" << std::endl
+				<< "\tFixed before operation is " << f1
+				//<< " with raw bits " << a.getRawBits()
+				<< std::endl;
+	for (int i = 0; i < 10; i++)
+		--f1;
+	std::cout 	<< "\tFixed after operation is " << f1
+				//<< " with raw bits " << a.getRawBits()
+				<< std::endl << std::endl;
 }
 
 int main( void )
@@ -47,35 +94,12 @@ int main( void )
 
 	a = Fixed( 1234.4321f );
 
-
-	std::cout 	<< "a is " << a
-				//<< " with raw bits " << a.getRawBits()
-				<< std::endl;
-	a++;
-	a++;
-	a++;
-	std::cout 	<< "a is " << a
-				//<< " with raw bits " << a.getRawBits()
-				<< std::endl;
-	std::cout 	<< "b is " << b
-				//<< " with raw bits " << b.getRawBits()
-				<< std::endl;
-	std::cout 	<< "c is " << c
-				//<< " with raw bits " << c.getRawBits()
-				<< std::endl;
-	std::cout 	<< "d is " << d
-				//<< " with raw bits " << d.getRawBits()
-				<< std::endl;
-
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-
+	test_show_fl(a, b, c, d);
+	test_show_int(a, b, c, d);
+	test_incr_decr(a);
 	test_comparison_operators(a, a);
 	test_arithmetic_operators(a, b);
 	test_minmax(a, b);
-	std::cout << "max is" << Fixed::min(a, a) << std::endl;
 
 	return (0);
 }
--- ex02/optests.cpp
+++ ex02/optests.cpp
@@ -6,7 +6,7 @@
 /*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 17:27:17 by jcummins          #+#    #+#             */
-/*   Updated: 2024/09/05 17:51:33 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/09/06 14:39:07 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,42 @@
 
 void test_greater(Fixed &f1, Fixed &f2) {
 	if (f1 > f2)
-		std::cout << "first is greater than second" << std::endl;
+		std::cout << "\tfirst is greater than second" << std::endl;
 	else
-		std::cout << "first is not greater than second" << std::endl;
+		std::cout << "\tfirst is not greater than second" << std::endl;
 }
 
 void test_lesser(Fixed &f1, Fixed &f2) {
 	if (f1 < f2)
-		std::cout << "first is lesser than second" << std::endl;
+		std::cout << "\tfirst is lesser than second" << std::endl;
 	else
-		std::cout << "first is not lesser than second" << std::endl;
+		std::cout << "\tfirst is not lesser than second" << std::endl;
 }
 
 void test_greater_eq(Fixed &f1, Fixed &f2) {
 	if (f1 >= f2)
-		std::cout << "first is greater than or equal to second" << std::endl;
+		std::cout << "\tfirst is greater than or equal to second" << std::endl;
 	else
-		std::cout << "first is not greater than or equal to second" << std::endl;
+		std::cout << "\tfirst is not greater than or equal to second" << std::endl;
 }
 
 void test_lesser_eq(Fixed &f1, Fixed &f2) {
 	if (f1 <= f2)
-		std::cout << "first is lesser than or equal to second" << std::endl;
+		std::cout << "\tfirst is lesser than or equal to second" << std::endl;
 	else
-		std::cout << "first is not lesser than or equal to second" << std::endl;
+		std::cout << "\tfirst is not lesser than or equal to second" << std::endl;
 }
 
 void test_equal(Fixed &f1, Fixed &f2) {
 	if (f1 == f2)
-		std::cout << "first is equal to second" << std::endl;
+		std::cout << "\tfirst is equal to second" << std::endl;
 	else
-		std::cout << "first is not equal to second" << std::endl;
+		std::cout << "\tfirst is not equal to second" << std::endl;
 }
 
 void test_unequal(Fixed &f1, Fixed &f2) {
 	if (f1 != f2)
-		std::cout << "first is unequal to second" << std::endl;
+		std::cout << "\tfirst is unequal to second" << std::endl;
 	else
-		std::cout << "first is not unequal to second" << std::endl;
+		std::cout << "\tfirst is not unequal to second" << std::endl;
 }
